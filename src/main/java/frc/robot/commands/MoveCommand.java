907401115b4d3b6c1f63fa2bf9bfdfeeae42ae9e
Melesio Albavera <melesio.albavera@acechaterhigh.org>
2019-12-13 08:12:26 -0800

    Added MoveCommand. Sorry for the error.

---
+++ src/main/java/frc/robot/commands/MoveCommand.java
@@ -0,0 +1,55 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.TestSubsystem;
+
+
+public class MoveCommand extends Command {
+	public enum Direction {
+		POS, NEG;
+	}
+	private TestSubsystem _test;
+	private double _dir;
+
+	public MoveCommand(Direction dir) {
+		super("MoveCommand");
+
+		_test = TestSubsystem.get_instance();
+		requires(_test);
+
+		_dir = dir == Direction.POS ? 1.0 : -1.0;
+	}
+
+
+	@Override
+	protected void initialize() {
+		execute();
+	}
+
+
+	@Override
+	protected void execute() {
+		_test.driveMotor(_dir);
+		//_test.printSwitchState();
+	}
+
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+
+	@Override
+	protected void end() {
+		_test.driveMotor(0);
+	}
+
+
+
+
+	@Override
+	protected void interrupted() {
+		super.interrupted();
+	}
+}
